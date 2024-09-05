d61bcd3ae99431717a244b7b67a80628bd571b54
Sergej Moor <sergejmoor01@gmail.com>
2024-09-05 13:23:07 +0200

    kinde auth setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-export default function Home() {
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
   return (
-    <div>
-      <div>Hello world</div>
-      <Button>Hello from te button</Button>
+    <div className="p-10 flex flex-col gap-y-4">
+      {session ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ) : (
+        <div>
+          <RegisterLink>
+            <Button>Register</Button>
+          </RegisterLink>
+          <LoginLink>
+            <Button>Login</Button>
+          </LoginLink>
+        </div>
+      )}
     </div>
   );
 }
